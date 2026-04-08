f30d41b2271953e084534e52fba1fd791bcb0109
Guille37778 <joseguerrero2189.a@gmail.com>
2026-04-08 02:35:44 -0400

    Limpieza profunda de archivos locales para Cloudflare

--- .vercel/output/_functions/chunks/supabase_CUGkxkOG.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://uvsbugnqskfiteqwdupp.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2c2J1Z25xc2tmaXRlcXdkdXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxOTcyNTQsImV4cCI6MjA5MDc3MzI1NH0.lBZp9vu91zj8J6K28FKamSpL5MXpYYf7WT5H-E2E0FM";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-const supabaseAdmin = supabase;
-
-export { supabaseAdmin as a, supabase as s };
