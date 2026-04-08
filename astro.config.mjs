1dd216ba47a4917aa445c77ce0be12b8535ac9bf
Guille37778 <joseguerrero2189.a@gmail.com>
2026-04-07 23:01:08 -0400

    migracion a claufare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 
-const isProduction = process.env.NETLIFY === 'true';
+const isProduction = process.env.CF_PAGES === 'true';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   
-  // Solo usamos el adaptador de Netlify en producción.
-  // En desarrollo (Local), usamos el servidor interno de Astro para evitar bloqueos en POST.
-  adapter: isProduction ? netlify() : undefined,
+  // Usamos el adaptador de Cloudflare para el despliegue.
+  adapter: cloudflare({ mode: 'directory' }),
 
   security: {
     // Deshabilitamos el chequeo de origen localmente para evitar errores 403 Forbidden
